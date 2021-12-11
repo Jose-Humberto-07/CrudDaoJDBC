1bf134b5c119d53de4d7e7444c80b650e22f9e2e
José Humberto <jhpnascimento96@gmail.com>
2021-12-11 12:39:32 -0300

    sellerdaojdbc and daofactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,10 @@
+package model.dao;
+
+import model.dao.implemetacao.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+}
