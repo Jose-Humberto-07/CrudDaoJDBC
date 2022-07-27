6909900052cfdfed08ef7285dc924a0be6284101
José Humberto <jhpnascimento96@gmail.com>
2022-07-26 22:00:46 -0300

    patch implements findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,12 @@
 package application;
 
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Seller;
@@ -8,11 +15,40 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+	/*
+	 * 	Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery("select * from seller");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name") + ", " + rs.getString("Email") + ", " + rs.getString("BirthDate") + ", " + rs.getString("BaseSalary") + ", " + rs.getString("DepartmentId"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	 * */
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = sellerDao.findById(3);
-		
+	
+		System.out.println("===== TEST ONE: seller findByID =====");
+		Seller seller = sellerDao.findById(2);
 		System.out.println(seller);
+		 
+		
+		
+		
 		
 	}
 
